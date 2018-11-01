903858ce076a1ac93afa7dc3629a0a004139b9e1
hiufungk <hiufungk@ualberta.ca>
2018-11-01 21:37:55 +0000

    added service, test service

---
+++ test_service.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from unittest.mock import patch
+from service import Service
+
+class TestService(TestCase):
+    def setUp(self):
+        self.service = Service()
+    
+    @patch("service.Service.bad_random", return_value=10)
+    def test_bad_random(self, mock_bad_random):
+        self.assertEqual(self.service.bad_random(),10)
+    
+    @patch("service.Service.bad_random", return_value=10)
+    def test_divide(self, mock_bad_random):
+        self.assertEqual(self.service.divide(2), 5)
+        try:
+            self.service.divide(0)
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+
+    def test_abs_plus(self):
+        self.assertEqual(self.service.abs_plus(-1), 2)
+        self.assertEqual(self.service.abs_plus(0), 1)
+        self.assertEqual(self.service.abs_plus(1), 2)
+    
+    @patch("service.Service.bad_random", return_value=10)    
+    def test_complicated_function(self, mock_bad_random):
+        rv1, rv2 = self.service.complicated_function(2)
+        self.assertEqual(rv1, 5)
+        self.assertEqual(rv2, 0)
+       
